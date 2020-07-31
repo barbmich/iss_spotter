7da09e6ff5ae67438dd87f4662cadde243640034
Michele Barbiero <babishhh@gmail.com>
2020-07-31 03:01:09 +0000

    added .then for fetchCoordsByIP in index2.js

--- index2.js
+++ index2.js
@@ -1,4 +1,5 @@
-const { fetchMyIP } = require('./iss_promised');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss_promised');
 
 fetchMyIP()
-    .then(body => console.log(body));
+    .then(fetchCoordsByIP)
+    .then(body => console.log(body))
